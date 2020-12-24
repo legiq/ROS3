dd9802b42bc4aa88de51907a67cfd07ae4699261
legiq <olegfom97@mail.com>
2020-12-24 11:53:14 +0300

    solution implemented

--- main.py
+++ main.py
@@ -10,15 +10,18 @@ class DontCrushDuckieTaskSolution(TaskSolution):
         env = self.generated_task['env']
         # getting the initial picture
         obs, _, _, _ = env.step([0,0])
-        # convect in for work with cv
-        img = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_BGR2RGB)
-        
-        # add here some image processing
         
         condition = True
         while condition:
+        
             obs, reward, done, info = env.step([1, 0])
+            # convect in for work with cv
             img = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_BGR2RGB)
-            # add here some image processing
-            condition = True
+            
+            mask = cv2.inRange(img, np.array([180,180,0]), np.array([255,255,150]))
+            
+            if (np.sum(mask) >= 0.1 * mask.shape[0] * mask.shape[1] * 255):
+                obs, reward, done, info = env.step([0, 0])
+                condition = False
+                
             env.render()
