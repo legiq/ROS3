e8051b6761c361d3892134c4ff434e852037e474
legiq <olegfom97@mail.com>
2020-12-24 12:15:38 +0300

    img fix and color fix

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class DontCrushDuckieTaskSolution(TaskSolution):
         while condition:
         
             obs, reward, done, info = env.step([1, 0])
-            img = cv2.cvtColor(np.ascontiguousarray(obs), cv2.COLOR_BGR2RGB)
+            img = np.ascontiguousarray(obs)
             
-            lower_color_bound = np.array([180,180,0])
+            lower_color_bound = np.array([190,180,0])
             upper_color_bound = np.array([255,255,150])
             
             mask = cv2.inRange(img, lower_color_bound, upper_color_bound)
             
-            if (np.sum(mask) >= 0.05 * mask.shape[0] * mask.shape[1] * 255):
+            if (np.sum(mask) > 0.05 * mask.shape[0] * mask.shape[1] * 255):
                 obs, reward, done, info = env.step([0, 0])
                 condition = False
                 
